57de59f4f576d2a30400f2e44d78e25fb56d4916
Amir Telmanov <wickedonefromkz@gmail.com>
2022-12-09 16:54:59 +0600

    second task

--- Program.cs
+++ Program.cs
@@ -3,16 +3,41 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number >= 100 & number < 1000)
+// if (number >= 100 & number < 1000)
+// {
+//     int res = number / 10;
+//     int res1 = res % 10;
+//     Console.WriteLine($"Вторая цифра числа {number} - {res1}");
+// }
+// else
+// {
+//     Console.WriteLine("Введено некорректное число");
+// }
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digitIndex = 2; // индекс нужной цифры
+
+if (number < 0)
 {
-    int res = number / 10;
-    int res1 = res % 10;
-    Console.WriteLine($"Вторая цифра числа {number} - {res1}");
+    number = -number;
+}
+
+if (number > 100)
+{
+    int digit = number.ToString()[digitIndex] - '0';
+    Console.WriteLine("Третья цифра " + digit + " //первым способом");
 }
 else
 {
-    Console.WriteLine("Введено некорректное число");
+    Console.WriteLine($"Во введенном числе {number} нет третьей цифры");
 }
