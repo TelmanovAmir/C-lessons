3c14a52ad3cea8a2ff8346567fd754c8f51876e9
Amir Telmanov <wickedonefromkz@gmail.com>
2022-12-11 19:10:40 +0600

    save

--- Program.cs
+++ Program.cs
@@ -47,9 +47,83 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите номер дня недели: ");
-int i = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите номер дня недели: ");
+// int i = Convert.ToInt32(Console.ReadLine());
 
-if (i >= 1 & i <= 5) Console.WriteLine("Будний день");
-if (i == 6 | i == 7) Console.WriteLine("Выходной");
-if (i > 7 | i < 1) Console.WriteLine("Такого дня не существует");
+// if (i >= 1 & i <= 5) Console.WriteLine("Будний день");
+// if (i == 6 | i == 7) Console.WriteLine("Выходной");
+// if (i > 7 | i < 1) Console.WriteLine("Такого дня не существует");
+
+
+
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+
+// if (N >= 10000 && N < 100000)
+// {
+//     int first = N.ToString()[0] - '0';
+//     int second = N.ToString()[1] - '0';
+//     int third = N.ToString()[2] - '0';
+//     int fourth = N.ToString()[3] - '0';
+//     int fifth = N.ToString()[4] - '0';
+//     if (first == fifth && second == fourth)
+//     {
+//         Console.WriteLine($"{N} является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{N} не является палиндромом");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Введено некорректное число");
+// }
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+// Console.WriteLine("Введите координаты точки A по x");
+// double xA = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки A по y");
+// double yA = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки A по z");
+// double zA = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки B по x");
+// double xB = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки B по y");
+// double yB = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки B по z");
+// double zB = Convert.ToDouble(Console.ReadLine());
+
+// double res = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+
+// Console.WriteLine($"{res:f2}");
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N");
+double N = Convert.ToDouble(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    double result = Math.Pow(i, 3);
+    Console.WriteLine(result);
+}
